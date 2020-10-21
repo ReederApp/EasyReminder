431b4d73e7c3535a9ae4b4a409ba322d74fd5ce3
Süleyman SEZER <me.iamcodder@gmail.com>
2020-10-22 01:53:01 +0300

    ViewBinding and fab button added

--- app/src/main/java/com/iamcodder/easyreminder/MainActivity.java
+++ app/src/main/java/com/iamcodder/easyreminder/MainActivity.java
@@ -1,14 +1,27 @@
 package com.iamcodder.easyreminder;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.iamcodder.easyreminder.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View mView = binding.getRoot();
+        setContentView(mView);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
